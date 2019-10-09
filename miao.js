30a2890eb529964706869edc4eb40385a4b63a1b
Wang.ch <Wang.ch@gaiasys.cn>
2019-10-09 14:10:06 +0800

    update

--- miao.js
+++ miao.js
@@ -7,7 +7,7 @@ function findAndCommit() {
     } else {
         console.warn('未能发现提交按钮!!!!');
         if (retry > 0) {
-            setTimeout(findAndCommit, 10);
+            setTimeout(findAndCommit, 1);
             retry--;
         }
     }
@@ -17,6 +17,12 @@ function auto() {
         //if (location.href.indexOf('buyertrade.taobao.com') > -1) {
         findAndCommit();
     }
+
+    if(location.href.indexOf('cart.taobao.com') > -1 || location.href.indexOf('cart.tmall.com') > -1){
+        checkout();
+    }
+
+    /**
     let start_time, end_time, server_time;
     let script2 = document.createElement('script');
     script2.innerHTML = 'function adjust_time(json){console.log(new Date(json.time*1000));console.log(new Date());}';
@@ -26,5 +32,32 @@ function auto() {
     script.async = false
     start_time = new Date().valueOf();
     document.body.appendChild(script);
+    */
+}
+var count = 1
+function checkout(){
+    if(document.querySelectorAll('.td-amount > .td-inner input')[0]==undefined){
+      setTimeout(checkout,50)
+      return
+    }
+    var value = document.querySelectorAll('.td-amount > .td-inner input')[0].value
+    while(value!=1){
+       document.querySelectorAll('.td-amount .J_Minus')[0].click()
+       value = document.querySelectorAll('.td-amount > .td-inner input')[0].value
+    }
+    
+    if(!document.querySelectorAll('.J_CheckBoxItem')[0].checked){
+      document.querySelectorAll('.td-chk > .td-inner > .cart-checkbox label')[0].click()
+    }
+    if(document.querySelectorAll('.J_CheckBoxItem')[0].checked){
+      console.log('checked')
+      if(count++ < 1000){
+        setTimeout(checkout,20)
+        return
+      }
+      document.querySelectorAll('.btn-area')[0].querySelectorAll('a')[0].click()
+    }
+    
+
 }
 auto();
